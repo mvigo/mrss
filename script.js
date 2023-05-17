e6247e36ddfd73b8535b76ffeb56a89a51953580
mvigo <manuel.vigo@gmail.com>
2023-05-17 17:51:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
+document.getElementById('load-feed').addEventListener('click', () => {
+  const feedUrl = document.getElementById('feed-url').value;
+  if (!feedUrl) return;
+
+  fetch(feedUrl)
+    .then(response => response.text())
+    .then(str => new DOMParser().parseFromString(str, "application/xml"))
+    .then(xmlDoc => displayVideos(xmlDoc))
+    .catch(err => {
+      alert('Error loading the MRSS feed. Please check your internet connection and try again.');
+      console.error(err);
+    });
+});
+
+function displayVideos(xmlDoc) {
+  const videoList = document.getElementById('videos');
+  videoList.innerHTML = '';
+
+  const items = xmlDoc.getElementsByTagName('item');
+  for (let i = 0; i < items.length; i++) {
+    const title = items[i].getElementsByTagName('title')[0].textContent;
+    const pubDate = items[i].getElementsByTagName('pubDate')[0]?.textContent;
+    const media = items[i].getElementsByTagName('media:content');
+    const thumbnail = items[i].getElementsByTagName('media:thumbnail')[0];
+    const keywords = items[i].getElementsByTagName('media:keywords')[0]?.textContent;
+    const description = items[i].getElementsByTagName('description')[0]?.textContent;
+
+    if (media.length > 0) {
+      const videoSources = Array.from(media).map(source => ({
+        url: source.getAttribute('url'),
+        type: source.getAttribute('type')
+      }));
+
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+        <img src="${thumbnail.getAttribute('url')}" alt="${title}">
+        <div>
+          <h3>${title}</h3>
+          <p>${pubDate ? pubDate : 'N/A'}</p> <!-- Added pubDate below the title -->
+        </div>
+      `;
+      listItem.addEventListener('click', () => playVideo(videoSources, title, pubDate, keywords, description));
+      videoList.appendChild(listItem);
+    }
+  }
+}
+
 function playVideo(videoSources, title, pubDate, keywords, description) {
   const player = document.getElementById('player');
   player.innerHTML = '';
 
   const video = document.createElement('video');
   video.controls = true;
-  player.appendChild(video);
-
-  const playbackSpeedSelector = document.createElement('select');
-  const speeds = [0.5, 1.0, 1.5, 2.0];
-
-  speeds.forEach(speed => {
-    const option = document.createElement('option');
-    option.value = speed;
-    option.text = `${speed}x`;
-    if (speed === 1.0) option.selected = true; // default speed
-    playbackSpeedSelector.appendChild(option);
-  });
-
-  playbackSpeedSelector.addEventListener('change', function() {
-    video.playbackRate = this.value;
-  });
-
-  player.appendChild(playbackSpeedSelector);
 
   for (const source of videoSources) {
     const sourceElement = document.createElement('source');
@@ -30,6 +59,7 @@ function playVideo(videoSources, title, pubDate, keywords, description) {
     video.appendChild(sourceElement);
   }
 
+  player.appendChild(video);
   video.play();
 
   const videoInfo = document.createElement('div');
